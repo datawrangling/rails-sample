521dc4acc7c57e96066f1c76e147c9f6783c9d33
Corey Johnson <probablycorey@gmail.com>
2009-04-04 19:59:13 -0400

    cap needs the reaper/spawner scripts

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,5 +5,6 @@ set :scm, :git
 set :repository,  "git://github.com/probablycorey/rails-sample.git"
 
 set :user, "mr_app"
+set :use_sudo, false
 
 server "ec2-174-129-222-40.compute-1.amazonaws.com", :app, :web, :db, :primary => true
