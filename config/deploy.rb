e64264bb84645dbb870c47750b7489c2b84d0e1e
Corey Johnson <probablycorey@gmail.com>
2009-04-04 21:35:48 -0400

    made deploy.rb more generic

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,4 +7,4 @@ set :repository,  "git://github.com/probablycorey/rails-sample.git"
 set :user, "mr_app"
 set :use_sudo, false
 
-server "ec2-174-129-222-40.compute-1.amazonaws.com", :app, :web, :db, :primary => true
+server "PUBLIC-DNS-OF-YOUR-EC2-INSTANCE", :app, :web, :db, :primary => true
