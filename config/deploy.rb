d27b06c160b902b8073159cd464e5c67c3316a46
Pete Skomoroch <pete@datawrangling.com>
2009-05-07 18:44:35 -0400

    modified github deploy to use ssh agent forwarding for use with github private repos, see http://blog.new-bamboo.co.uk/2008/3/12/github-with-capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,10 +1,13 @@
 set :application, "sample"
-set :deploy_to, "/data/apps/#{application}"
 
 set :scm, :git
-set :repository,  "git://github.com/probablycorey/rails-sample.git"
+set :repository,  "git://github.com/datawrangling/rails-sample.git"
+set :repository_cache, "git_cache"
+set :deploy_via, :remote_cache
+set :ssh_options, { :forward_agent => true }
 
 set :user, "mr_app"
 set :use_sudo, false
 
 server "PUBLIC-DNS-OF-YOUR-EC2-INSTANCE", :app, :web, :db, :primary => true
+set :deploy_to, "/data/apps/#{application}"
